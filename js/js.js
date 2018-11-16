13256e32b0d989d608c2d38a49b6d062cac6fad8
xxxWALDxxx <43649644+xxxWALDxxx@users.noreply.github.com>
2018-11-16 23:10:07 +0300

    16.11.2018
    Правки Димы

--- js/js.js
+++ js/js.js
@@ -12,3 +12,33 @@ $(document).ready(function(){
     menu.toggleClass('menu-active');
   });
 });
+function changeItem_item1() {
+  document.getElementById('imac-img-display').style.background = 'green';
+}
+function changeItem_item2() {
+  document.getElementById('imac-img-display').style.background = 'white';
+}
+function changeItem_item3() {
+  document.getElementById('imac-img-display').style.background = '#222222';
+}
+function changeItem_item4() {
+  document.getElementById('imac-img-display').style.background = '#333333';
+}
+function changeItem_item5() {
+  document.getElementById('imac-img-display').style.background = '#444444';
+}
+function changeItem_item6() {
+  document.getElementById('imac-img-display').style.background = '#666666';
+}
+function changeItem_item7() {
+  document.getElementById('imac-img-display').style.background = 'green';
+}
+function changeItem_item8() {
+  document.getElementById('imac-img-display').style.background = 'black';
+}
+function changeItem_item9() {
+  document.getElementById('imac-img-display').style.background = 'yellow';
+}
+function rechangeItem() {
+  document.getElementById('imac-img-display').style.background = 'transparent';
+}
